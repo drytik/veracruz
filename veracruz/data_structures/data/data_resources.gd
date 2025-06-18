18f0a211afb813f547dae47ad9aedc625d2ef59b
drytik <logicadri@gmail.com>
2025-06-18 08:32:46 +0200

    Day9
    Updated data resources

--- veracruz/data_structures/data/data_resources.gd
+++ veracruz/data_structures/data/data_resources.gd
@@ -1,7 +1,37 @@
 class_name DataResources
 extends Resource
 
-@export var gold : int = 0
+## Primarios 
+
 @export var wood : int = 0 
-@export var food : int = 0 
+@export var fruits : int = 0 
+@export var cotton : int = 0 
+@export var corn : int = 0 
+@export var dyes : int = 0 
+@export var cocoa : int = 0
+@export var clay : int = 0 
 @export var stone : int = 0 
+@export var gold : int = 0
+@export var agave : int = 0
+@export var silver : int = 0
+
+## Secundarios 
+
+@export var planks : int = 0
+@export var cloth : int = 0
+@export var flour : int = 0
+@export var beverages : int = 0
+@export var iron : int = 0
+@export var tools : int = 0
+@export var gold_bars : int = 0
+@export var population : int = 0
+@export var welfare : int = 0
+# Real de a 8 @export var planks : int = 0
+
+## Foráneos
+
+@export var livestock : int = 0
+@export var weapons : int = 0
+@export var glass : int = 0
+@export var books : int = 0
+@export var pottery : int = 0
